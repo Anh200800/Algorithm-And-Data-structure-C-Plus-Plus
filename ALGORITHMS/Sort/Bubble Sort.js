7017b95c80f8a998a3075902f61aa4c9ddf4af0c
Your Name <you@example.com>
2022-06-16 17:23:02 +0700

    leetcode

---
+++ ALGORITHMS/Sort/Bubble Sort.js
@@ -0,0 +1,5 @@
+// Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in the wrong order. 
+// This algorithm is not suitable for large data sets as its average and worst case time complexity is quite high.
+
+// Bubble Sort là thuật toán sắp xếp đơn giản nhất hoạt động bằng cách hoán đổi nhiều lần các phần tử liền kề nếu chúng không đúng thứ tự. 
+// Thuật toán này không phù hợp với các tập dữ liệu lớn vì độ phức tạp thời gian trung bình và trường hợp xấu nhất của nó là khá cao.
